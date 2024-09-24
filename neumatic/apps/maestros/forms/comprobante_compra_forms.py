9e29096cf49d913ba7c6117163c16e71c78f0a7a
Leoncio R. Barrios H <leonciobarriosh@gmail.com>
2024-09-24 13:07:33 -0500

    Añadido comentario en el módulo comprobante_compra.py

--- neumatic/apps/maestros/forms/comprobante_compra_forms.py
+++ neumatic/apps/maestros/forms/comprobante_compra_forms.py
@@ -3,6 +3,8 @@ from ..models.base_models import ComprobanteCompra
 from diseno_base.diseno_bootstrap import (
     formclasstext, formclassselect, formclassdate)
 
+
+# Este es un comentario por Leoncio.
 class ActividadForm(forms.ModelForm):
     
     class Meta:
