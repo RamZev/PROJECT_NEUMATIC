d8e9215e795ee8057ce7783f285c0d9cfdc0079d
Ramosric <neuraldevs@gmail.com>
2024-09-24 13:25:08 -0500

    cuarto commit

--- neumatic/apps/maestros/forms/comprobante_compra_forms.py
+++ neumatic/apps/maestros/forms/comprobante_compra_forms.py
@@ -1,13 +1,10 @@
-## neumatic\apps\maestros\forms\comprobante_compra_forms.py
+# neumatic\apps\maestros\forms\comprobante_compra_forms.py
 from django import forms
 from ..models.base_models import ComprobanteCompra
 from diseno_base.diseno_bootstrap import (
     formclasstext, formclassselect, formclassdate)
 
-# Este es un comentario por Ricardo.
 
-# Este es un comentario por Leoncio.
-# Esta es otra línea de comentario.
 class ActividadForm(forms.ModelForm):
     
     class Meta:
