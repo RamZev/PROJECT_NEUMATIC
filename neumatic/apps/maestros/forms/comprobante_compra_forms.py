0b2ee38d057386e7886cccc3e7291efffc9748fc
Ramosric <neuraldevs@gmail.com>
2024-09-23 23:35:18 -0500

    Segundo commit

---
+++ neumatic/apps/maestros/forms/comprobante_compra_forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from ..models.base_models import ComprobanteCompra
+from diseno_base.diseno_bootstrap import (
+    formclasstext, formclassselect, formclassdate)
+
+class ActividadForm(forms.ModelForm):
+    
+    class Meta:
+        model = ComprobanteCompra
+        fields = '__all__'
+
+        widgets = {}
