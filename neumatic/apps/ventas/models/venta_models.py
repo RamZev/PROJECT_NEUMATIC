bbcfff3befdda1b4db29d4176e93ed91de9b20c3
Leoncio R. Barrios H. <leonciobarriosh@gmail.com>
2025-08-14 16:19:07 -0500

    Registro modelo venta.StockCliente en app venta

--- neumatic/apps/ventas/models/venta_models.py
+++ neumatic/apps/ventas/models/venta_models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from apps.maestros.models.base_models import ModeloBaseGenerico
 
-
 class StockCliente(ModeloBaseGenerico):
 	id_stock_cliente = models.AutoField(primary_key=True)
 	id_factura = models.ForeignKey(
